f9cea0e5c63a016cbe3955b25b089acf1c2da174
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-16 18:46:00 -0700

    First c program to use puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main() {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+/**
+ * main - entry point
+ *
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
