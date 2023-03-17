4de95f4d928214970c302b0ae75223fa2dcc2aaf
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-17 15:16:07 -0700

    check value of n

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n == 0)
+	{
+		printf("%d and is 0\n", n);
+	}
+	else if (n > 5)
+	{
+		printf("%d and is greater than 5\n", n);
+	}
+	else if (n < 6)
+	{
+		printf("%d and is less than 6 and not 0\n", n);
+	}
+	return (0);
+}
