179759a4a6f75513f030f2c330fdd25ed9d6452d
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-17 16:47:38 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int i = 'A' ; i <= 'Z' ; i++)
+	{
+		char small = tolower(i);
+
+		putchar(small);
+	}
+	return (0);
+}
