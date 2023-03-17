41ca022f606fccc7b8aabf7892bbd3163dc49983
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-17 15:03:03 -0700

    Check n value

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,26 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
 
-int main (void)
+/* betty style doc for function main goes there */
+int main(void)
 {
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
 	if (n > 0)
 	{
-		printf("is positive");
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("is negative");
+		printf("%d is negative\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("is zero");
+		printf("%d is zero\n", n);
 	}
+	return (0);
 }
