744e6179f68961a5b0a367a104ec9e83b68edee1
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-16 19:12:55 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -8,9 +9,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
