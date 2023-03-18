dc249fd301345548f793334291e8628065995008
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-17 17:13:49 -0700

    Print lower then Upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int i;
+        char alpha[26] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        for (i = 0; i < 52 ; i++)
+        {
+                putchar(alpha[i]);
+        }
+        putchar('\n');
+        return (0);
+}
