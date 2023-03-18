4de57727be4afc2e70197c3bc0f8d261020cd101
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-17 18:11:22 -0700

    reverse alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char alpha[26] = "zyxvutsrqpomnlkjihgfedcba";
+
+	for (i = 26; i <= 0 ; i--)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
