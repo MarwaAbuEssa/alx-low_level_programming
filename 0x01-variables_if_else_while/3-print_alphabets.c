86ebaf4247a41cd79a0ae5d4ebd6e970659b2903
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-17 17:17:35 -0700

    Print lower then Upper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,13 +10,13 @@
 
 int main(void)
 {
-        int i;
-        char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	 int i;
+	 char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-        for (i = 0; i < 52 ; i++)
-        {
-                putchar(alpha[i]);
-        }
-        putchar('\n');
-        return (0);
+	for (i = 0; i < 52 ; i++)
+	{
+	 putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
 }
