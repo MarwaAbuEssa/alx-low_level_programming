a3f253c4559f161df7abe2782dae57f6112bb4b5
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-23 14:19:32 -0700

    5 more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,12 +9,19 @@
 
 void more_numbers(void)
 {
-	int i, mult;
+	int i, mult = 0;
 
-	for (i = 0 ; i <= 14 ; i++)
+	while (mult <= 10)
 	{
-		mult = i * 10;
-		putchar((mult / 10) + '0');
+		for (i = 0 ; i <= 14 ; i++)
+		{
+			if (i > 10)
+			{
+				putchar((i / 10) + '0');
+			}
+			putchar((i % 10) + '0');
+		}
+		mult++;
+		putchar('\n');
 	}
-	putchar('\n');
 }
