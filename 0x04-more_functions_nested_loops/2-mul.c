532976d535ad90f8d1e826dc04cb0fa82496da16
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-23 12:41:41 -0700

    digits multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - function to check if digit
+ *
+ * @a: paramter of int
+ * @b: second param of int
+ * Return: return int
+ */
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
