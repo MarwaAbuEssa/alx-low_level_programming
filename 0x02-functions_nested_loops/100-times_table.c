62ba0d13eb1cf3beaf6e56bbf5c707e0ca277221
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-22 18:12:52 -0700

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,8 +19,13 @@ void print_times_table(int n)
 			for (j = 0 ; j <= n ; j++)
 			{
 				prod  = i * j;
+				if (prod =< 10 )
+					_putchar(prod + '0')
+				else
+				{
 				_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
+				}
 				if (j != n)
 				{
 					_putchar(',');
