0b96df31808304ee5a9783f25c524ccefb7b4f96
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-24 19:42:52 -0700

    max fact

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,25 +8,17 @@
 
 int main(void)
 {
-	long int n;
+	long n;
 
 	n = 612852475143;
-	long int div = 2, maxFact;
+	long div, maxFact;
+	double square = sqrt(number);
 
-	while (n != 0)
+	while (div <= square)
 	{
-		if (n % div != 0)
-			div += 1;
-		else
-		{
-			maxFact  = div;
-			n = n / div;
-			if (n == 1)
-			{
-				printf("%lu is the largest prime factor\n", maxFact);
-				break;
-			}
-		}
+		if (n % div == 0)
+			maxFact = n / div;
 	}
+	printf("%d\n", maxFact);
 	return (0);
 }
