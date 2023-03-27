23360af91dc1d3c383ecde68e85c6a86e9d7c4cd
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-27 14:21:48 -0700

    swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - pointer to an as aparameter
+ * @a: pointer first value
+ * @b: pointer for second value
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *temp = &a;
+	*a = *b;
+	*b = *temp;
+}
