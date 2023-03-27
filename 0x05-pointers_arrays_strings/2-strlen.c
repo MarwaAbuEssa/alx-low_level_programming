798f4031dea337e2b2a91b3330a95e6ebde8742d
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-27 14:52:29 -0700

    strlen function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - pointer to an as aparameter
+ * @s: pointer first value
+ * Return: return length as integer
+ */
+
+int _strlen(char *s)
+{
+	char i;
+	int len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+	return (len);
+}
