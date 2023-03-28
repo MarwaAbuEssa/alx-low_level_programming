64743bb9a105fab1b1c4eaf22f45d4fd929f9c4b
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-27 17:29:48 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,7 +12,7 @@ void print_rev(char *s)
 	int i, len, temp;
 
 	len = strlen(s);
-	for (i = 0; i < len / 2; i++)
+	for (i = len - 1; i >= len / 2; i--)
 	{
 		temp = s[i];
 		s[i] = s[len - i - 1];
