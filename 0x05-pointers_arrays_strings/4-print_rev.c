437f9b1506599c928a95698c34697110d96ee668
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-27 17:24:58 -0700

    resverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - pointer to an as aparameter
+ * @s: pointer first value
+ * Return: return length as integer
+ */
+
+void print_rev(char *s)
+{
+	int i, len, temp;
+
+	len = strlen(s);
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+	 printf("%s\n", s);
+}
