0c50d4d44203305a580a2de35770e75ae40b711c
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-28 18:18:09 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - Entry point
+ * Return: Always Success
+ */
+
+int main(void)
+{
+	int myrand, count, total;
+
+	srand(time(NULL));
+	for (count = 0; total = 2772; totla > 122; count++)
+	{
+		myrand = (rand() % 125) + 1;
+		printf("%c", myrand);
+		total -= myrand;
+	}
+	printf("%c", total);
+
+	return (0);
+}
