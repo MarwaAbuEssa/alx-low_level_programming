42f7184779b9bd7dffdeabd7c991f9969e5fe803
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-30 16:58:58 -0700

    string compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strcmp - strcmp from src to dest
+ * @s1: pointer param
+ * @s2: pointer second
+ * Return: return concat pointer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result, i;
+
+	for (i = 0; s1[i] == s2[i] && s1[i] != '\0'; i++)
+	{
+		if (s1[i] == s2[i])
+			result = 0;
+		else
+			result = s1[i] - s2[i];
+	}
+	return (result);
+}
