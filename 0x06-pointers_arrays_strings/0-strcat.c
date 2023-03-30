8400c8424823718b27dcc709cef081616bd17171
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-30 04:51:39 -0700

    concatenate 2 string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,12 +4,21 @@
  *_strcat - pointer to an as aparameter
  * @dest: pointer param
  * @src: pointer second
+ * Return: return concat pointer
  */
 
 char *_strcat(char *dest, char *src)
 {
-	char concat = (*dest) + (*src);
+	int i = 0, j = 0;
 
-	dest = &concat;
+	while (dest[i] != '\0')
+	i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 	return (dest);
 }
