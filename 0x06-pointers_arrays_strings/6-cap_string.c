bebd256625508bc17c5ed9851819c08d0383cc60
MarwaAbuEssa <meromeroo@gmail.com>
2023-03-30 18:38:18 -0700

    Capatialize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - strcmp from src to dest
+ * @s: char param
+ * Return: return char in upper case
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+			i++;
+
+				if (s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n'
+				|| s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.'
+				|| s[i - 1] == '!' || s[i - 1] == '?' ||
+				s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')'
+				|| s[i - 1] == '{' || s[i - 1] == '}' || i == 0)
+				s[i] = s[i] - 32;
+
+			i++;
+	}
+
+	return (s);
+}
