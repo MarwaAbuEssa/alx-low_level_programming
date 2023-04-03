e6123cbd235ace00fe958ac9c15d7a5ebc4e2b1e
MarwaAbuEssa <meromeroo@gmail.com>
2023-04-03 15:20:35 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_memsett - pointer to an as aparameter
+ *_memset - pointer to an as aparameter
  * @s: pointer to the memory
  * @b: character to fill
  * @n: number of bytes
@@ -10,7 +10,7 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	int i;
 
 	for (i = 0; i < n; i++)
 		s[i] = b;
