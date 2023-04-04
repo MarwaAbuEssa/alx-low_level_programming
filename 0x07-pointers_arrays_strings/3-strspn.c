bb4ce4826bc14831bfeba230222679ba309163e2
MarwaAbuEssa <meromeroo@gmail.com>
2023-04-03 18:31:07 -0700

    string spa

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *_strspn - get the length of a prefix substring
+ * @s: A pointer to the memory area to search
+ * @accept: prefix
+ * Return: A pointer to the result memory
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	unsigned int bytes = 0;
+
+	while (*s)
+	{
+		for (i = 0; accept[i] ; i++)
+		{
+			if (*s == accept[i])
+			{
+				bytes++;
+				break;
+			}
+		}
+		s++;
+	}
+
+	return (bytes);
+}
