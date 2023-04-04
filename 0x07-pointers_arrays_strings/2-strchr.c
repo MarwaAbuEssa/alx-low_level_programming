7f6c71efd55638f57cae9586475762bdf602a311
MarwaAbuEssa <meromeroo@gmail.com>
2023-04-03 17:27:03 -0700

    Search charachter

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strchr - pointer to an as aparameter
+ * @s: A pointer to the memory area to search
+ * @c: charachter to search in memory
+ * Return: A pointer to the result memory
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+	char *result;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			result = &s[i];
+	}
+
+	return (result);
+}
