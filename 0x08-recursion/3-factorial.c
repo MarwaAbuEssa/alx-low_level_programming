bbca9524a5013ec5ec68b9316751494f0dacec2d
MarwaAbuEssa <meromeroo@gmail.com>
2023-04-05 18:23:20 -0700

    fractional

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - fractinoal of number
+ * @n: char as input
+ * Return: return length
+ */
+
+int factorial(int n)
+{
+
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+
+	return (n * fractional(n - 1));
+}
