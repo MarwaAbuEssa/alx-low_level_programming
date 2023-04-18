5b3a216bf6f6ebec53d46e6bac36a48c84180ec8
MarwaAbuEssa <meromeroo@gmail.com>
2023-04-17 17:30:55 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - dog type
+ *@name: The name
+ *@age: The age
+ *@owner: The owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ *dog_t - Typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+
+#endif
