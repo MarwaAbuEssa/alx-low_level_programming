6f14f04960e7ff4715212a647aae5681d1a2efbe
MarwaAbuEssa <meromeroo@gmail.com>
2023-05-02 12:57:02 -0700

    Add note at the end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,8 +21,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 
-	new->next = NULL;
 	new->n = n;
+	new->next = NULL;
 
 	if (*head == NULL)
 		*head = new;
