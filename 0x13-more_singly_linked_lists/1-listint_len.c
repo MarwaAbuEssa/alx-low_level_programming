94d864decb328b42afaab81d429eeb6405d5e2c9
MarwaAbuEssa <meromeroo@gmail.com>
2023-05-02 12:09:53 -0700

    number of nodes

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+
+/**
+ * listint_len - return nodes
+ * @h: header pointer
+ * Return: number of nodes
+ */
+
+
+size_t listint_len(const listint_t *h)
+{
+	size_t number_of_nodes = 0;
+
+	while (h != NULL)
+	{
+		number_of_nodes++;
+		h = h->next;
+	}
+	return (number_of_nodes);
+}
