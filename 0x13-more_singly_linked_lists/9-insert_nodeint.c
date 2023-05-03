635f22ef3dd09d7849ef739d3f46566e94681b72
MarwaAbuEssa <meromeroo@gmail.com>
2023-05-02 19:00:23 -0700

    Insert at position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,8 +33,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (ptr == NULL || ptr->next == NULL)
 			return (NULL);
 
-		ptr = ptr->next;
 		idx--;
+		ptr = ptr->next;
 	}
 	new->next = ptr->next;
 	ptr->next = new;
