f1c3dbc406b0271e6c4614d299f6db3ba8942773
MarwaAbuEssa <meromeroo@gmail.com>
2023-05-02 18:48:42 -0700

    Delete node at position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,8 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
-
-	idx--;
+	
 	while (idx != 1)
 	{
 		if (ptr == NULL || ptr->next == NULL)
