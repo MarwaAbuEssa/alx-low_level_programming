476eb0ca64890e6c944412b58547f8f16b185ce8
MarwaAbuEssa <meromeroo@gmail.com>
2023-05-03 18:04:09 -0700

    fix in add node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,10 +30,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	
 	while (idx != 1)
 	{
+		idx--;
 		if (ptr == NULL || ptr->next == NULL)
 			return (NULL);
-
-		idx--;
 		ptr = ptr->next;
 	}
 	new->next = ptr->next;
