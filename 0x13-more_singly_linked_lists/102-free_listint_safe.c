679506b4bc1fbffdc92a37c145473b8229625784
MarwaAbuEssa <meromeroo@gmail.com>
2023-08-01 17:08:11 -0700

    betty 102 free

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -5,7 +5,7 @@ size_t free_listint_safe(listint_t **h);
 
 /**
  * looped_listint_count - Counts the number of unique nodes
- * @head: A pointer to the head of the listint_t to check.    
+ * @head: A pointer to the head of the listint_t to check  
  * Return: If the list is not looped - 0.
  */
 size_t looped_listint_count(listint_t *head)
@@ -49,7 +49,7 @@ size_t looped_listint_count(listint_t *head)
 }
 
 /**
- * free_listint_safe - Frees a listint_t list safely 
+ * free_listint_safe - Frees a listint_t list safely
  * @h: A pointer to the address of
  * Return: The size of the list that was freed.
  * Description: The function sets the head to NULL.
