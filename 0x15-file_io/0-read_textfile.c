ed1e5d41bb1dd6c21c6f9e57b6c4733f1404aed5
MarwaAbuEssa <meromeroo@gmail.com>
2023-08-07 17:55:44 -0700

    read file and read me

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - read text from file
+ * @filename: path pf file to read data
+ * @letters: number of charchters to read
+ * Return: 0 if fails or number of charchaters
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t openFile, readFile, writeFile;
+	char *theBuffer;
+
+	if (filename == NULL)
+		return (0);
+
+	theBuffer = malloc(sizeof(char) * letters);
+	if (theBuffer == NULL)
+		return (0);
+
+	openFile = open(filename, O_RDONLY);
+	readFile = read(openFile, theBuffer, letters);
+	writeFile = write(STDOUT_FILENO, theBuffer, readFile);
+
+	if (openFile == -1 || readFile == -1 ||
+			writeFile == -1 || writeFile != readFile)
+	{
+		free(theBuffer);
+		return (0);
+	}
+
+	free(theBuffer);
+	close(openFile);
+
+	return (writeFile);
+}
