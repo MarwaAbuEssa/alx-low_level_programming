9b327b5eb935cc249fb08a3c93796346fd1b8c94
MarwaAbuEssa <meromeroo@gmail.com>
2023-09-14 12:51:51 -0700

    print number of nodes

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - return node elemets
+ * @h: header pointer
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+
+}
