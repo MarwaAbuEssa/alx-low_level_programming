bb000350e9c29ad143735a084312f7ff20fef4ef
MarwaAbuEssa <meromeroo@gmail.com>
2023-09-14 13:10:59 -0700

    get length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - return node elemets
+ * @h: header pointer
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+
+}
